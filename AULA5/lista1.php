b97a6ec2264f9e5c4c8e2a8909cfb896fa4a75e5
Gabriel Fonseca <gabrielramooos08@gmail.com>
2026-03-25 13:43:20 -0300

    Update lista1.php

--- AULA5/lista1.php
+++ AULA5/lista1.php
@@ -1,11 +1,18 @@
 <?php
 
-$araia = array ();
-for ($i=0; $i < 10; $i++) {
-    $num = readline($i + 1 . "° Insira um número: ");
-    array_push($araia,$num);
+$media = [];
+for ($i = 1; $i <= 10; $i++) {
+    $num = readline("informe um número: ");
+    array_push($media, $num);
 }
 
-$media = ($araia[0] + $araia[1] + $araia[2] + $araia[3] + $araia[4] + $araia[5] + $araia[6] + $araia[7] + $araia[8] + $araia[9]) / 10;
+$tam = count($media);
 
-echo "\nA média do vetor é: " . $media;
+$soma = 0;
+foreach ($media as  $aritmetica) {
+    $soma = $soma + $aritmetica;
+}
+
+$mediatotal = $soma / $tam;
+
+echo "A media total é: " . $mediatotal . "\n";
