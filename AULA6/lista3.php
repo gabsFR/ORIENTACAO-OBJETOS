22cb826d9a224d73482afddf5d1826d6f0338041
Gabriel Fonseca <gabrielramooos08@gmail.com>
2026-04-08 14:55:13 -0300

    Refactor functions to accept array parameters

--- AULA6/lista3.php
+++ AULA6/lista3.php
@@ -2,7 +2,7 @@
 
 //functions
 
-function soma($gru)
+function soma(array $gru)
 {
     $soma = 0;
     foreach ($gru as $pepsiman) {
@@ -11,7 +11,7 @@ function soma($gru)
     return $soma;
 }
 
-function par($gru)
+function par(array $gru)
 {
     $pares = 0;
     foreach ($gru as $pinduca) {
@@ -20,6 +20,60 @@ function par($gru)
         }
     }
     return $pares;
+}
+
+function impar(array $gru)
+{
+    $impares = 0;
+    foreach ($gru as $pinduca) {
+        if ($pinduca % 2 != 0) {
+            $impares++;
+        }
+    }
+    return $impares;
+}
+
+function divFive(array $gru)
+{
+
+    $elcinco = 0;
+    foreach ($gru as $pinduca) {
+        if ($pinduca % 5 == 0) {
+            $elcinco++;
+        }
+    }
+    return $elcinco;
+}
+
+function shroom(array $gru)
+{
+
+    $menor = $gru[0];
+    $maior = $gru[0];
+
+    //Encontra o menor
+    foreach ($gru as $v) {
+        if ($v < $menor)
+            $menor = $v;
+    }
+
+    //Encontra o maior
+    foreach ($gru as $v) {
+        if ($v > $maior)
+            $maior = $v;
+    }
+
+    return $maior - $menor;
+}
+
+function qtdTresSeisNove(array $gru) {
+    $qtdTresSeisNove = 0;
+    foreach($gru as $v) {
+        if($v == 3 || $v == 6 || $v == 9)
+            $qtdTresSeisNove++;
+    }
+    
+    return $qtdTresSeisNove;
 }
 
 //main
@@ -37,9 +91,14 @@ echo "\nA SOMA DE TODOS OS NUMEROS É: " . $soma . "\n";
 $par = par($gru);
 echo "\nTEM " . $par . " NUMEROS PARES";
 // 3. impares
-
+$impar = impar($gru);
+echo "\nTEM " . $impar . " NUMEROS IMPARES";
 // 4. divisiveis por 5
-
+$ps5 = divFive($gru);
+echo "\nTEM " . $ps5 . " DIVISIVEIS POR 5";
 // 5. maior e menor
-
+$luigi = shroom($gru);
+echo "\nA DIFERENÇA ENTRE O MAIOR E O MENOR É: " . $luigi;
 // 6. 3, 6 e 9
+$sixnine = qtdTresSeisNove($gru);
+echo "\nA QUANTIDADE DE 3, 6 E 9 SÃO: " . $sixnine . "\n";
